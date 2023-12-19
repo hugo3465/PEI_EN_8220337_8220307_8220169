0fb88d72fea6d9f465ffe758a4d16a0d255ba42e
Hugo Guimarães <hugo.ricardo.almeida.guimaraes@gmail.com>
2023-12-19 13:59:12 +0000

    setting up MongoDB

---
+++ PEI_EN_8220337_8220307_8220169/MongoDB/Passing Data from Csv Collections To Final Collections.js
@@ -0,0 +1,8 @@
+// Select the database to use.
+use('BdPeiTP');
+
+// customer_csv, address_csv, country_csv, city_csv -> to Customer
+
+
+
+// product_csv, category_csv, sub_category_csv, sub_category_product_csv -> to Product
