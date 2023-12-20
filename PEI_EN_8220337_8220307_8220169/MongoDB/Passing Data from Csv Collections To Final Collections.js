b50d395c47c09255dfd1bdd4d959fc8220677330
Hugo Guimarães <hugo.ricardo.almeida.guimaraes@gmail.com>
2023-12-20 08:20:03 +0000

    finalização da pipeline dos clientes

--- PEI_EN_8220337_8220307_8220169/MongoDB/Passing Data from Csv Collections To Final Collections.js
+++ PEI_EN_8220337_8220307_8220169/MongoDB/Passing Data from Csv Collections To Final Collections.js
@@ -2,7 +2,7 @@
 use('BdPeiTP');
 
 // customer_csv, address_csv, country_csv, city_csv -> to Customer
-
+db.customers_csv.find({})
 
 
 // product_csv, category_csv, sub_category_csv, sub_category_product_csv -> to Product
