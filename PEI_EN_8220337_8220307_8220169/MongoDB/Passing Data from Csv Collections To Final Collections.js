548eaf9ab0b797128f241e38f8962933fd9b078e
Hugo Guimarães <hugo.ricardo.almeida.guimaraes@gmail.com>
2023-12-21 16:58:42 +0000

    finalização do protótipo da query do relatóriode vendas
    Apenas um protótipo, é preciso adaptá-la, quando ela for para a API

--- PEI_EN_8220337_8220307_8220169/MongoDB/Passing Data from Csv Collections To Final Collections.js
+++
@@ -1,8 +0,0 @@
-// Select the database to use.
-use('BdPeiTP');
-
-// customer_csv, address_csv, country_csv, city_csv -> to Customer
-db.customers_csv.find({})
-
-
-// product_csv, category_csv, sub_category_csv, sub_category_product_csv -> to Product
